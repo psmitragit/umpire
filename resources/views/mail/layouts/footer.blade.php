c8d5da988d0fb1846d6426c5f0d761c62f4e0506
Ayan <karmakarayan217@gmail.com>
2024-04-25 12:53:33 +0530

    unsub style added

--- resources/views/mail/layouts/footer.blade.php
+++ resources/views/mail/layouts/footer.blade.php
@@ -2,9 +2,10 @@
     @php
         $encMail = Illuminate\Support\Facades\Crypt::encryptString($unsubMail);
     @endphp
-    <div>
-        <a href="{{ url('unsubscribe/' . $encMail) }}">Unsubscribe</a>
+    <div style="margin-top: 10px; margin-bottom: 10px;">
+        <a href="{{ url('unsubscribe/' . $encMail) }}" style="text-decoration: none; color: #ffffff; background-color: #a41515; padding: 10px 20px; border-radius: 5px; display: inline-block;">Unsubscribe</a>
     </div>
+
 @endif
 </body>
 
