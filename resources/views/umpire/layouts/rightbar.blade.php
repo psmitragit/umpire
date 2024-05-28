878f7a488909e431479c6801fd117940fcc4ffd9
ambarkundu <140590491+ambarkundu@users.noreply.github.com>
2024-05-28 10:22:50 +0530

    side bar scroll fixed

--- resources/views/umpire/layouts/rightbar.blade.php
+++ resources/views/umpire/layouts/rightbar.blade.php
@@ -1,34 +1,36 @@
 <div class="pos-abes-righ-sidebar">
 
-    <div class="upcoming-games">
+    <div class="upcoming-games asff">
         <div class="upcomingpages-flex">
             <h2>Upcoming Games</h2>
             <a href="{{ url('umpire') }}" class="textreds">View All</a>
         </div>
-        @if ($upc_games = umpire_upcoming_games(3))
-            @foreach ($upc_games as $upc_game)
-                @php
-                    $upc_game_inputDate = Illuminate\Support\Carbon::parse($upc_game->gamedate_toDisplay);
-                    $upc_game_dateFormatted = $upc_game_inputDate->format('D n/j');
-                    $upc_game_timeFormatted = $upc_game_inputDate->format('g:ia');
-                @endphp
-                <div class="datesd">
-                    <span class="texts-with-icon"><i class="fa-solid fa-calendar-days"></i>
-                        {{ $upc_game_dateFormatted }}</span>
-                    <span class="texts-with-icon"><i class="fa-solid fa-clock"></i> {{ $upc_game_timeFormatted }}</span>
-                    <span class="texts-with-icon"><i class="fa-solid fa-users"></i> {{ $upc_game->playersage }}
-                        Yrs</span>
-                </div>
+        <div class="games-upcomings">
+            @if ($upc_games = umpire_upcoming_games(3))
+                @foreach ($upc_games as $upc_game)
+                    @php
+                        $upc_game_inputDate = Illuminate\Support\Carbon::parse($upc_game->gamedate_toDisplay);
+                        $upc_game_dateFormatted = $upc_game_inputDate->format('D n/j');
+                        $upc_game_timeFormatted = $upc_game_inputDate->format('g:ia');
+                    @endphp
+                    <div class="datesd">
+                        <span class="texts-with-icon"><i class="fa-solid fa-calendar-days"></i>
+                            {{ $upc_game_dateFormatted }}</span>
+                        <span class="texts-with-icon"><i class="fa-solid fa-clock"></i>
+                            {{ $upc_game_timeFormatted }}</span>
+                        <span class="texts-with-icon"><i class="fa-solid fa-users"></i> {{ $upc_game->playersage }}
+                            Yrs</span>
+                    </div>
 
 
-                <div class="heading">
-                    <i class="fa-solid fa-location-dot"></i>
-                    <span class="pointer-location">{{ $upc_game->location->ground }}</span>
+                    <div class="heading">
+                        <i class="fa-solid fa-location-dot"></i>
+                        <span class="pointer-location">{{ $upc_game->location->ground }}</span>
 
-                </div>
-            @endforeach
-        @endif
-
+                    </div>
+                @endforeach
+            @endif
+        </div>
         @if (checkIfUmpireNeedsToSubmitReport() > 0)
             <a href="{{ url('umpire/show-reports') }}" class="blue-btns view-btn primart-yehs">Submit Reports</a>
         @endif
