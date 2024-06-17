3ac761e238d7b2e1933a1cae05d05f3506f9584c
Ayan <karmakarayan217@gmail.com>
2024-06-17 13:23:41 +0530

    demo umpires added to elague

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -11,6 +11,7 @@ use App\Models\UmpirePrefModel;
 use App\Models\LeagueUmpireModel;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
+use App\Models\LeagueEmailSettingsModel;
 use App\Models\UmpireEmailSettingsModel;
 
 class DemoController extends Controller
@@ -62,9 +63,10 @@ class DemoController extends Controller
             }
         }
     }
-    public function autoApproveLeagueApplication($id){
+    public function autoApproveLeagueApplication($id, $leagueId = 1)
+    {
 
-        $league_data = LeagueModel::find(1);
+        $league_data = LeagueModel::find($leagueId);
         $umpire = UmpireModel::find($id);
 
         $existingRecord = LeagueUmpireModel::where('umpid', $id)
@@ -93,17 +95,67 @@ class DemoController extends Controller
         ];
         UmpirePrefModel::create($umpPref_data);
         //adding to umpire pref
-        try {
-            $msg = 'You joined ' . $league_data->leaguename;
-            add_notification($id, $msg, 2, 'ump');
-            Session::flash('message', 'Application approved by league.');
-        } catch (\Throwable $th) {
+        if ($leagueId == 1) {
+            try {
+                $msg = 'You joined ' . $league_data->leaguename;
+                add_notification($id, $msg, 2, 'ump');
+                Session::flash('message', 'Application approved by league.');
+            } catch (\Throwable $th) {
+            }
         }
     }
     public function demoLeague()
     {
         if (logged_in_league_data()) {
             return redirect('league');
+        }
+
+        $maxId = UserModel::max('uid');
+        $newId = $maxId + 1;
+
+        $email = "demoleague$newId@yopmail.com";
+        $password = $email;
+
+        $user_data = [
+            'email' => $email,
+            'password' => Hash::make($password),
+            'isLeagueOwner' => 1,
+            'usertype' => 2
+        ];
+        if ($user = UserModel::create($user_data)) {
+            $cc = getUniqueColor();
+            $league_data = [
+                'name' => "Demo League $newId",
+                'phone' => '123-123-1234',
+                'leaguename' => "Demo League $newId",
+                'status' => 1,
+                'cc' => $cc,
+            ];
+            try {
+                $league = LeagueModel::create($league_data);
+                $user->leagueid = $league->leagueid;
+                $user->save();
+                $emailSettings_data = [
+                    'leagueid' => $league->leagueid
+                ];
+                LeagueEmailSettingsModel::create($emailSettings_data);
+
+                //adding demo umpires
+
+                $demoUmpIds = [132, 133, 134, 135, 136, 137, 138, 139, 140];
+
+                foreach ($demoUmpIds as $demoUmpId) {
+                    $this->autoApproveLeagueApplication($demoUmpId, $league->leagueid);
+                }
+
+                //adding demo umpires
+
+                session(['league_data' => $user]);
+                return redirect('league');
+            } catch (Exception $e) {
+                UserModel::find($user->uid)->delete();
+            }
+        } else {
         }
     }
 }
