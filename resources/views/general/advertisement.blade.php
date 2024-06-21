2f57a6260401dc4bf8d949d3ad0ad5dfbd92ec8f
ambarkundu <140590491+ambarkundu@users.noreply.github.com>
2024-06-21 10:12:09 +0530

    banner ad

---
+++ resources/views/general/advertisement.blade.php
@@ -0,0 +1,24 @@
+@extends('general.layouts.main')
+@section('main-container')
+<main>
+    <div class="home-banner addpage">
+        <div class="banner-tecxts">
+            <h1 class="banner-title"><span>Products </span> Central</h1>
+            <div class="texr-banne">The first scheduling solution exclusively for Baseball
+            </div>
+        </div>
+
+        <div class="overkaycolor"></div>
+        <div class="overkaycolor2"></div>
+    </div>
+    <div class="logo">
+        <div class="logo-uc">
+            <img src="{{ asset('storage/frontend/image/uc-logo.png') }}" alt="">
+        </div>
+    </div>
+
+
+</main>
+
+
+@endsection
